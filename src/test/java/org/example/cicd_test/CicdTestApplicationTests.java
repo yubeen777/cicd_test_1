bfb92dd867fdea53ed316944bc581fcf2b016a63
yubeen777 <panda7715@naver.com>
2025-06-23 12:06:37 +0900

    🎉 최초 커밋

---
+++ src/test/java/org/example/cicd_test/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
